1e005e913bfe7abf8eef64bf5a6537e9b8bb61d8
fuck <asdgfsg235@gmail.com>
2023-12-30 18:16:05 +0300

    second commit

---
+++ 14/14.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		a = 1
+		s = "abc"
+		f = 15.1
+		b = true
+	)
+
+	fmt.Println(typeof(a))
+	fmt.Println(typeof(s))
+	fmt.Println(typeof(f))
+	fmt.Println(typeof(b))
+
+	fmt.Println(typeofSC(a))
+	fmt.Println(typeofSC(s))
+	fmt.Println(typeofSC(b))
+}
+
+// with sprintf with %T spec
+func typeof(v interface{}) string {
+	return fmt.Sprintf("type of variable : %T", v) // %T stands for var type
+}
+
+// with switch-case
+func typeofSC(v interface{}) string {
+	switch v.(type) {
+	case int:
+		return "int"
+	case float64:
+		return "float64"
+	//... etc
+	default:
+		return "unknown"
+	}
+}
---
+++ 6/6.go
@@ -0,0 +1,95 @@
+package main
+
+/*
+Task : implement all possible ways to stop goroutines
+*/
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// goroutine can't be stopped by another goroutine
+func main() {
+	launcFirst()
+	launchSecond()
+	launchThird()
+	fmt.Println("Main Goroutine executed")
+}
+
+// launch & test all 3 goroutines with 3 funcs
+func launcFirst() {
+	stopChan := make(chan struct{})
+	go firstWay(stopChan)
+	time.Sleep(3 * time.Second)
+	stopChan <- struct{}{} // can use empty structs, lightweight way for cases like this
+	// close(stopChan) // works similar i guess but can be used once
+	fmt.Println("First launch ended")
+}
+
+func launchSecond() {
+	var (
+		stop bool
+		wg   sync.WaitGroup
+	)
+	wg.Add(1)
+	go secondWay(&stop, &wg)
+	time.Sleep(3 * time.Second)
+	stop = true
+	wg.Wait()
+	fmt.Println("Second launch ended")
+}
+
+func launchThird() {
+	ctx, cancel := context.WithCancel(context.Background()) // todo - read about different contexts types
+	go thirdWay(ctx)
+	time.Sleep(3 * time.Second)
+	cancel() // this sends smthng to signal and tells goroutine to select case with exit
+	time.Sleep(time.Second)
+	fmt.Println("Third launch ended")
+}
+
+// first way to stop g-tine, using channel with select case, with ch <- or channel close()
+func firstWay(stCh chan struct{}) {
+	for {
+		select {
+		case <-stCh: // if something is coming from stCh, this case will be executed
+			fmt.Println("First goroutine stopped")
+		default:
+			fmt.Println("Running first goroutine")
+			time.Sleep(time.Second)
+		}
+
+	}
+}
+
+// second way - using shared variable with its condition check
+func secondWay(stop *bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		if *stop {
+			fmt.Println("Second goroutine stopped")
+			return
+		}
+
+		fmt.Println("Running second goroutine")
+		time.Sleep(time.Second)
+	}
+}
+
+// third way - using context
+func thirdWay(ctx context.Context) {
+	for { // infinite loop
+		select {
+		case <-ctx.Done(): // if signal channel sent something, this case will be selected
+			fmt.Println("Third goroutine stopped")
+			return
+		default: // if no cancellation signals (in this case) -> just sleep and move on
+			fmt.Println("Third goroutine running")
+			time.Sleep(time.Second)
+		}
+	}
+}
